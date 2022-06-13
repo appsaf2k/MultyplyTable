1067d053304daf5fdf12d25dc96b8aa1ddbbe532
appsaf2k <appsaf2k@gmail.com>
2022-06-13 11:52:22 +0300

    Initial Commit

---
+++ MultyplyTable/MultyplyTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultyplyTableApp.swift
+//  MultyplyTable
+//
+//  Created by @andreev2k on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultyplyTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
