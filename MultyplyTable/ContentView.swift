c037d7bade457159df49f938081530d40aef7b4a
appsaf2k <appsaf2k@gmail.com>
2022-07-22 09:13:11 +0300

    Mini application for learning multiplication

--- MultyplyTable/ContentView.swift
+++ MultyplyTable/ContentView.swift
@@ -8,9 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animals = ["bear","buffalo","chik","chicken","cow","crocodile","dog","duck","elephant","frog","giraffe","goat","gorilla","hippo","horse","monkey","moose","narwhal","owl","panda","parrot","penguin","pig","rabbit","rhino","sloth","snake","walrus","whale","zebra"].shuffled()
+    
+    
+    @State private var number = Int.random(in: 2...12)
+    @State private var numbers = Int.random(in: 2...12)
+    
+    @State private var correctTitle = ""
+    
+    @State private var showingFail = false
+    
+    @State private var selected = -1
+    @State private var amount = ""
+    
+    
+    var multiply: Int {
+        let multiPly = number * numbers
+        return multiPly
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Form {
+                    Section {
+                        Text("Выбери число")
+                            .font(.title.bold())
+                        
+                        Stepper("Число     \(number)", value: $number, in: 2...12)
+                            .font(.title2.bold())
+                    } header: {
+                        Text("")
+                    }
+                    
+                    Text("Сколько будет       \(number)  х  \(numbers)")
+                        .font(.title2.bold())
+                    
+                    
+                    Section {
+                        TextField("ОТВЕТ", text: $amount)
+                            .padding()
+                            .font(.title.bold())
+                        
+                    
+                            HStack {
+                                Spacer()
+                                Button {
+                                    tapped(Int(self.amount) ?? selected)
+                                } label: {
+                                    Image(animals[9])
+                                       
+                                }
+                                Spacer()
+                            }
+                    }
+                } .navigationBarTitle("Умножение")
+            }
+            
+            .alert(correctTitle, isPresented: $showingFail) {
+                Button("Продолжить", action: askQuestion)
+            }  message: {
+                Text("") }
+        }
+    }
+    
+    
+    func askQuestion() {
+        animals.shuffle()
+        selected = -1
+        numbers = Int.random(in: 2...12)
+    }
+    
+    func tapped(_ number: Int) {
+        selected = number
+        if number == multiply {
+            correctTitle = "Верно, МОЛОДЕЦ!"
+            showingFail = true
+        } else {
+            correctTitle = "HE Верно, правильный ответ:  \(multiply)"
+            showingFail = true
+        }
+        
     }
 }
 
